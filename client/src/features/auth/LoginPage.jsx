cb581051cafac38cb73ece354c8e40405413cc7b
EvegeniyNekrasov <eanekrasovart@gmail.com>
2025-07-31 23:04:13 +0200

    adding tanstack form to client

--- client/src/features/auth/LoginPage.jsx
+++ client/src/features/auth/LoginPage.jsx
@@ -1,54 +1,125 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useAuth } from "../../context/AuthContext";
 import { useNavigate } from "react-router";
+import { useForm } from "@tanstack/react-form";
+
+function FieldInfo({ field }) {
+    return (
+        <>
+            {field.state.meta.isTouched && !field.state.meta.isValid ? (
+                <em>{field.state.meta.errors.join(",")}</em>
+            ) : null}
+            {field.state.meta.isValidating ? "Validating..." : null}
+        </>
+    );
+}
 
 const LoginPage = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
     const { user, login } = useAuth();
     const navigate = useNavigate();
+
+    const form = useForm({
+        defaultValues: {
+            username: "",
+            password: "",
+        },
+        onSubmit: async ({ value }) => {
+            console.log(value);
+            const success = await login(value.username, value.password);
+            if (success) navigate("/");
+            else alert("Invalid credentials");
+        },
+    });
 
     useEffect(() => {
         if (user) navigate("/");
     }, [user, navigate]);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const success = await login(username, password);
-        if (success) {
-            navigate("/");
-        } else {
-            alert("Invalid credentials");
-        }
-    };
-
     return (
         <div>
             <h2>Iniciar sesión</h2>
-            <form onSubmit={handleSubmit}>
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    form.handleSubmit();
+                }}>
                 <div>
-                    <label>
-                        Usuario:
-                        <input
-                            type="text"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                            required
+                    <form.Field
+                        name="username"
+                        validators={{
+                            onChange: ({ value }) =>
+                                !value
+                                    ? "A first name is required"
+                                    : value.length < 3
+                                    ? "First name must be at least 3 characters"
+                                    : undefined,
+                            onChangeAsyncDebounceMs: 500,
+                        }}
+                        children={(field) => {
+                            return (
+                                <>
+                                    <label htmlFor={field.name}>
+                                        Username:
+                                    </label>
+                                    <input
+                                        id={field.name}
+                                        name={field.name}
+                                        value={field.state.value}
+                                        onBlur={field.handleBlur}
+                                        onChange={(e) =>
+                                            field.handleChange(e.target.value)
+                                        }
+                                    />
+                                    <FieldInfo field={field} />
+                                </>
+                            );
+                        }}
+                    />
+                    <div>
+                        <form.Field
+                            name="password"
+                            children={(field) => (
+                                <>
+                                    <label htmlFor={field.name}>
+                                        Password:
+                                    </label>
+                                    <input
+                                        id={field.name}
+                                        name={field.name}
+                                        value={field.state.value}
+                                        onBlur={field.handleBlur}
+                                        onChange={(e) =>
+                                            field.handleChange(e.target.value)
+                                        }
+                                    />
+                                    <FieldInfo field={field} />
+                                </>
+                            )}
                         />
-                    </label>
+                    </div>
                 </div>
-                <div>
-                    <label>
-                        Contraseña:
-                        <input
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                        />
-                    </label>
-                </div>
-                <button type="submit">Iniciar sesión</button>
+                <form.Subscribe
+                    selector={(state) => [state.canSubmit, state.isSubmitting]}
+                    children={([canSubmit, isSubmitting]) => (
+                        <>
+                            <button
+                                type="submit"
+                                disabled={!canSubmit}>
+                                {isSubmitting ? "..." : "Submit"}
+                            </button>
+                            <button
+                                type="reset"
+                                onClick={(e) => {
+                                    // Avoid unexpected resets of form elements (especially <select> elements)
+                                    e.preventDefault();
+                                    form.reset();
+                                }}>
+                                Reset
+                            </button>
+                        </>
+                    )}
+                />
             </form>
         </div>
     );
